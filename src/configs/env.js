75a275ed94de55998c685b19f0b451ea83c0ce54
ikbs-rs <ikbs.rs@gmail.com>
2023-08-26 17:48:44 +0200

    ...

--- src/configs/env.js
+++ src/configs/env.js
@@ -3,11 +3,11 @@ const env = {
     START_URL: 'http://ws10.ems.local:8351',
     ADM_URL: 'http://ws10.ems.local:8352',
     CMN_URL: 'http://ws10.ems.local:8353',
-    JWT_BACK_URL: 'http://ws11.ems.local:8302',
-    ADM_BACK_URL: 'http://ws11.ems.local:8302',
-    CMN_BACK_URL: 'http://ws11.ems.local:8303',   
-    TIC_BACK_URL: 'http://ws11.ems.local:8304', 
-    IMG_BACK_URL: 'http://ws11.ems.local:8305',    
+    JWT_BACK_URL: 'http://ws10.ems.local:8302',
+    ADM_BACK_URL: 'http://ws10.ems.local:8302',
+    CMN_BACK_URL: 'http://ws10.ems.local:8303',   
+    TIC_BACK_URL: 'http://ws10.ems.local:8304', 
+    IMG_BACK_URL: 'http://ws10.ems.local:8305',    
   };
   
   export default env;
