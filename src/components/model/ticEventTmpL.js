d03de50de969133b8eec902f2aa9579125c0224c
ikbs-rs <ikbs.rs@gmail.com>
2023-11-08 03:40:03 +0100

    ...

--- src/components/model/ticEventTmpL.js
+++ src/components/model/ticEventTmpL.js
@@ -17,11 +17,7 @@ import DateFunction from '../../utilities/DateFunction';
 import ConfirmDialog from '../dialog/ConfirmDialog';
 
 export default function TicEventL(props) {
-<<<<<<< Updated upstream
-console.log("*********TicEventL*********", props)
-=======
 console.log(props, "********************TicEventLTmp*********************************")
->>>>>>> Stashed changes
     let i = 0;
     const objName = 'tic_event';
     const selectedLanguage = localStorage.getItem('sl') || 'en';
