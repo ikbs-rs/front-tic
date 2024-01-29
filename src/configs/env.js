772ff87bc86e89ce11fb6bc77976acc6ee6a0bbd
ikbs-rs <ikbs.rs@gmail.com>
2024-01-29 22:29:34 +0100

    ...

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,26 +1,15 @@
 const env = {
-<<<<<<< Updated upstream
-    DOMEN: 'http://localhost',
-    START_URL: 'http://localhost:8351/start/',
-    ADM_URL: 'http://localhost:8352/adm/',
-    CMN_URL: 'http://localhost:8353/cmn/',
-    TIC_URL: 'http://localhost:8354/tic/',
-    JWT_BACK_URL: 'http://localhost:8302/badm',
-    ADM_BACK_URL: 'http://localhost:8302/badm',
-    CMN_BACK_URL: 'http://localhost:8303/bcmn',
-    TIC_BACK_URL: 'http://localhost:8304/btic',
-    IMG_BACK_URL: 'http://localhost:8305/img',
-=======
-    START_URL: 'https://82.117.213.106/start/',
-    ADM_URL: 'https://82.117.213.106/adm/',
-    CMN_URL: 'https://82.117.213.106/cmn/',
-    TIC_URL: 'https://82.117.213.106/tic/',
-    JWT_BACK_URL: 'https://82.117.213.106/badm',
-    ADM_BACK_URL: 'https://82.117.213.106/badm',
-    CMN_BACK_URL: 'https://82.117.213.106/bcmn',
-    TIC_BACK_URL: 'https://82.117.213.106/btic',
-    IMG_BACK_URL: 'https://82.117.213.106/bimg',
->>>>>>> Stashed changes
+    DOMEN: 'http://192.168.72.96',
+    START_URL: 'http://192.168.72.96/start/',
+    ADM_URL: 'http://192.168.72.96/adm/',
+    CMN_URL: 'http://192.168.72.96/cmn/',
+    TIC_URL: 'http://192.168.72.96/tic/',
+    JWT_BACK_URL: 'http://192.168.72.96/badm',
+    ADM_BACK_URL: 'http://192.168.72.96/badm',
+    CMN_BACK_URL: 'http://192.168.72.96/bcmn',
+    TIC_BACK_URL: 'http://192.168.72.96/btic',
+    IMG_BACK_URL: 'http://192.168.72.96/bimg',
   };
-
+  
   export default env;
+  
