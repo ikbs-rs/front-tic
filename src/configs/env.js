6af7e8dbcf1ffa3bf4e1a4005d705fc62e15271d
ikbs-rs <ikbs.rs@gmail.com>
2024-02-04 21:43:47 +0100

    ...

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,14 +1,14 @@
 const env = {
-    DOMEN: 'https://dev.app.ems.rs',
-    START_URL: 'https://dev.app.ems.rs/start/',
-    ADM_URL: 'https://dev.app.ems.rs/adm/',
-    CMN_URL: 'https://dev.app.ems.rs/cmn/',
-    TIC_URL: 'https://dev.app.ems.rs/tic/',
-    JWT_BACK_URL: 'https://dev.app.ems.rs/badm',
-    ADM_BACK_URL: 'https://dev.app.ems.rs/badm',
-    CMN_BACK_URL: 'https://dev.app.ems.rs/bcmn',
-    TIC_BACK_URL: 'https://dev.app.ems.rs/btic',
-    IMG_BACK_URL: 'https://dev.app.ems.rs/bimg',
+    DOMEN: 'http://localhost:8351',
+    START_URL: 'http://localhost:8351/start/',
+    ADM_URL: 'http://localhost:8352/adm/',
+    CMN_URL: 'http://localhost:8353/cmn/',
+    TIC_URL: 'http://localhost:8354/tic/',
+    JWT_BACK_URL: 'http://localhost:8302/badm',
+    ADM_BACK_URL: 'http://localhost:8302/badm',
+    CMN_BACK_URL: 'http://localhost:8303/bcmn',
+    TIC_BACK_URL: 'http://localhost:8304/btic',
+    IMG_BACK_URL: 'http://localhost:8305/img',
   };
   
   export default env;
